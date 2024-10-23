0ee7138bb8135f29cf97a4598579de98ca4b12ea
joseach116 <joseluisamadochacon@gmail.com>
2024-10-23 09:55:51 +0000

    domain

--- src/app.js
+++ src/app.js
@@ -16,7 +16,8 @@ window.onload = function() {
   for (let i = 0; i < pronoun.length; i++) {
     for (let x = 0; x < adj.length; x++) {
       for (let y = 0; y < noun.length; y++) {
-        console.log(pronoun[i] + adj[x] + noun[y] + ".com");
+        let domain = pronoun[i] + adj[x] + noun[y] + ".com";
+        console.log(domain);
       }
     }
   }
